5779ebcfbe16679772de680ce9c33f37261830e9
Mateusz Kłakus <mati.klakus@gmail.com>
2023-09-30 19:40:58 +0200

    feat: ShowPicker method

---
+++ src/pages/ShowPicker.tsx
@@ -0,0 +1,41 @@
+import { useRef } from 'react';
+
+const ShowPicker = () => {
+  const datePickerRef = useRef<HTMLInputElement>(null);
+
+  const handleShowPicker = () => {
+    if (!datePickerRef.current) {
+      return;
+    }
+
+    datePickerRef.current.showPicker();
+  };
+
+  return (
+    <section className='flex flex-col items-center justify-center gap-4 py-24'>
+      <h1 className='text-3xl font-bold tracking-tighter text-white sm:text-5xl xl:text-6xl/none '>
+        showpicker()
+      </h1>
+      <div className='w-[300px] p-6 bg-white shadow-md rounded-lg flex flex-col'>
+        <label htmlFor='browser' className='mb-2 font-semibold text-gray-700'>
+          Choose your browser from the list:
+        </label>
+        <input
+          ref={datePickerRef}
+          name='date-picker'
+          id='date-icker'
+          type='date'
+          className='w-full px-3 py-2 text-black border border-gray-300 rounded-md focus:outline-none focus:border-blue-500'
+        />
+        <button
+          className='px-3 py-2 mx-auto mt-5 text-black border border-gray-300 rounded-md bg-slate-300 focus:outline-none focus:border-blue-500'
+          onClick={handleShowPicker}
+        >
+          Show Picker
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default ShowPicker;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
 export { default as Home } from './Home';
 export { default as DataList } from './DataList';
+export { default as ShowPicker } from './ShowPicker';
