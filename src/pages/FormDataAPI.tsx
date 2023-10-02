d771037d15ef22d2a60e61bec73eb868f5fbee32
Mateusz Kłakus <mati.klakus@gmail.com>
2023-10-02 16:20:49 +0200

    fix: changed button style

--- src/pages/FormDataAPI.tsx
+++ src/pages/FormDataAPI.tsx
@@ -56,7 +56,7 @@ const ContentEditable = () => {
           <div className='text-center'>
             <button
               type='submit'
-              className='bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600'
+              className='px-3 py-2 mx-auto mt-5 text-black border border-gray-300 rounded-md bg-slate-300 focus:outline-none focus:border-blue-500'
             >
               Submit
             </button>
